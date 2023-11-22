85a537ed09389bc7af2e04a1fa95beb770a51579
zukio <as.web554@gmail.com>
2023-11-22 18:20:00 +0900

    package update

--- plugins/option.js
+++ plugins/option.js
@@ -4,12 +4,16 @@
 // ウィンドウの設定
 const userConfig = {
   windowConfig: {
+    fullscreen: false,
     width: 800,
     height: 600,
+    x: 0, // 画面の左端
+    y: 0, // 画面の上端
     webPreferences: {
       devTools: true,
     },
     titleBarStyle: "visible", //"hidden",
+    alwaysOnTop: true, // 最前面に表示
     frame: true,
   },
   ignoreMouseEvents: false,
